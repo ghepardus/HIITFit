5353ec03843082b56a1caa98caa6561c07bb2071
Mario Severino <mario.severino1986@gmail.com>
2022-05-24 09:04:07 +0200

    Cap 2 - WIP

---
+++ HIITFit/Views/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+      TabView {
+          Text("Welcome")
+          Text("Exercise 1")
+          Text("Exercise 2")
+      }
+      .tabViewStyle(PageTabViewStyle())
+      .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
